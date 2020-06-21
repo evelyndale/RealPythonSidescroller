e0c532275c2227540fb49c42f9688b2154e087e9
Evelyn <evelynvioletdale@gmail.com>
2020-06-21 10:46:13 -0700

    add movement

--- sky_dodge.py
+++ sky_dodge.py
@@ -19,13 +19,23 @@ SCREEN_HEIGHT = 600
 
 # Define a player object by etending pygame.sprite.Sprite
 # The surface drawn on the screen is now an attribute of 'player'
-class Player(pygame.sprite.Sprite)
+class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
         self.surf = pygame.Surface((75, 25))
         self.surf.fill((255, 255, 255))
-        self.rect = self.surf.get
+        self.rect = self.surf.get_rect()
 
+        #Move the sprite based on user keypresses
+     def update(self, pressed_keys):
+        if pressed_keys[K_UP]:
+           self.rect.move_ip(0, -5) 
+        if pressed_keys[K_DOWN]:
+           self.rect.move_ip(0, 5) 
+        if pressed_keys[K_LEFT]:
+           self.rect.move_ip(-5, 0) 
+        if pressed_keys[K_RIGHT]:
+           self.rect.move_ip(5, 0) 
 
 # Initialize pygame
 pygame.init()
@@ -52,20 +62,13 @@ while running:
         elif event.type == QUIT:
             running = False
 
+    pressed_keys = pygame.key.get_pressed()
+
     #Fill the screen with white
     screen.fill((0, 0, 0))
 
-    #Create a surface and pass in a tuple containing its length and width
-    surf = pygame.Surface((50, 50))
+    # Draw the player on the screen
+    screen.blit(player.surf, player.rect)
 
-    #Give the surface a color to separate it from the background
-    surf.fill((0, 0, 0))
-    rect = surf.get_rect()
-
-    surf_center = (
-            (SCREEN_WIDTH-surf.get_width())/2,
-            (SCREEN_HEIGHT-surf.get_height())/2
-    )
-    # This line says "Draw surf on to the screen on the center"
-    screen.blit(surf, surf_center)
+    #update the display
     pygame.display.flip()
